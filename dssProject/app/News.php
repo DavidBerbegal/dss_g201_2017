3505488beaa256d0401668db398880d31de83f0f
Álvaro <eb76@alu.ua.es>
2017-03-20 12:35:47 +0100

    Eliminados atributos de modelos

--- dssProject/app/News.php
+++ dssProject/app/News.php
@@ -6,20 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-   protected $table = 'news';
-   protected $primaryKey = 'idNew';
-   private $author;
-   private $title;
-   private $date;
-   private $description;
-   private $urlNew;
-   private $urlImg;
-   private $positiveRate;
-   private $negativeRate;
-   private $source;
-   private $category;
-   private $language;
-   private $country;
 
    public function bookmarks()
    {
--- dssProject/app/User.php
+++ dssProject/app/User.php
@@ -7,11 +7,7 @@ use Illuminate\Foundation\Auth\User as Model;
 
 class User extends Model
 {
-    protected $primaryKey='idUser';
-    private $password;
-    private $email;
-    public $name;
-    
+  
     public function bookmarks()
     {
         return $this -> hasMany('App\Bookmark');
