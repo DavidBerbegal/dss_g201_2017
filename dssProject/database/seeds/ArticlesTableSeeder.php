9b4c475e6853fab63ba9557cb0476048ae525137
leorodmrez <lrm56@alu.ua.es>
2017-03-30 12:03:22 +0200

    añadido control de error en la consulta HTTP a la API

--- dssProject/database/seeds/ArticlesTableSeeder.php
+++ dssProject/database/seeds/ArticlesTableSeeder.php
@@ -12,34 +12,18 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-         //borramos los datos de la tabla
-       
-        // Añadimos una entrada a esta tabla
-       /* DB::table('articles')->insert([
-            'author' => 'Charles Dickens', 
-            'title' => 'Title prueba 1',
-            'description' => 'Descripiton prueba 1', 
-            'urlNew' => 'www.prueba1.com', 
-            'urlImg' => 'www.prueab1/img.com',
-            'date' => '2016-11-11',
-            'positiveRate' => '1000',
-            'negativeRate' => '180',
-            'category_id' => 1,
-            'source_id' => 1,
-            'language' => 'en',
-            'country' => 'Spain'
-         ]);*/
-
+        //hacemos la consulta http a la api
          $content = file_get_contents("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=a522b74c74f64e2daa03511d0e80ae6f");
-         //echo $content;
-         $jsonContent = json_decode($content);
-         //echo $jsonContent->status;
+         
+        //insertamos el contenido resultante de la consulta en un objeto JSON
+        $jsonContent = json_decode($content);   
+         
+         //comprobamos que el estado de la consulta http ha sido "ok" (200)
          if ($jsonContent->status = "ok"){
-             $i = 0;
+
             foreach ($jsonContent->articles as $article) {
                 //echo $article->title;
-               if($i <= 2){
-                    $i++;
+                    
                     DB::table('articles')->insert([
                     'author' => $article->author, 
                     'title' => $article->title,
@@ -49,14 +33,15 @@ class ArticlesTableSeeder extends Seeder
                     'date' => $article->publishedAt,
                     'positiveRate' => '0',
                     'negativeRate' => '0',
-                    'category_id' => $i,
-                    'source_id' => $i,
+                    'category_id' => '1',
+                    'source_id' => '1',
                     'language' => 'en',
                     'country' => 'Spain'
-                    ]);
-                }
-         
+                    ]);   
             }
+         }
+         else{
+             echo "La consulta HTTP a la API ha fallado";
          }
     }
 }
