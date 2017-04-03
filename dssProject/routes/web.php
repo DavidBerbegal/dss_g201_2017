a1c1c89d43d9966a2ee3aaa7f58389c5dc6fb0d7
DavidBerbegal <davidbdecastalla@hotmail.com>
2017-04-03 11:28:31 +0200

    Actualización de la ruta de las fuentes

--- dssProject/routes/web.php
+++ dssProject/routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::get('/', function () {
     return view('index');
 });
@@ -27,9 +23,9 @@ Route::get('/categorias', function () {
     return view('categorias');
 });
 
-Route::get('/fuentes', function () {
-    return view('fuentes');
-});
+Route::get('fuentes', [
+    'uses' => 'controllerSources@index'
+]);
 
 Route::get('/suscripcion-categorias', function () {
     return view('suscripcionCategorias');
