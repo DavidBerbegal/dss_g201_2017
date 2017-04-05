25bc192d2bd552446344cfdabfd639614f96c127
DavidBerbegal <davidbdecastalla@hotmail.com>
2017-04-05 23:32:18 +0200

    Corrección de errores varios

--- dssProject/routes/web.php
+++ dssProject/routes/web.php
@@ -48,7 +48,9 @@ Route::get('/fuentes/nuevaFuente', function() {
 
 Route::post('/fuentes/nuevaFuente', 'controllerSources@create');
 
-Route::get('/fuentes/modificarFuente', 'controllerSources@update');
+Route::get('/fuentes/modificarFuente', 'controllerSources@showSource');
+
+Route::post('/fuentes/modificarFuente', 'controllerSources@update');
 
 // Rutas Sucripción Categoría
 Route::get('/suscripcion-categorias', function () {
