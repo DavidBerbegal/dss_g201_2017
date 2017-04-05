bae996f14c3fea9bb392317ba3e13cb591be0cd8
Álvaro Esteve <eb76@alu.ua.es>
2017-04-05 18:19:10 +0200

    Solucion conflicto #4

--- dssProject/resources/views/usuarios.blade.php
+++ dssProject/resources/views/usuarios.blade.php
@@ -75,7 +75,7 @@
                     <button type="submit" name="buscar">Buscar</button>
                 </form></div>
 
-                <table class="table table-hover">
+                <table class="flex-center table table-hover">
                     <thead>
                         <tr>
                             <th><h4>ID:</h4></th>
